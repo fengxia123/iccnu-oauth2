46b9851456478af5252181525e4d0e19bce35261
zhonghua <ccnuyan@live.com>
2015-08-10 10:03:09 +0800

    new images

--- dot net CS Resource Owner Password Credentials Grant/ICCNU.ClientCredentialGrant/Program.cs
+++ dot net CS Resource Owner Password Credentials Grant/ICCNU.ClientCredentialGrant/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
-using ClouDeveloper.Mime;
 using DotNetOpenAuth.OAuth2;
-using System.Net.Http;
 using Newtonsoft.Json;
 
 namespace ICCNU.ClientCredentialGrant
--- dot net CS Resource Owner Password Credentials Grant/ICCNU.MySite/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICCNU.MySite.MvcApplication" Language="C#" %>
